5dd00f8ea917a6b1afa61f7e14483be71e9c82bf
ecasdia <cassio.dias.cassiminho@ericsson.com>
2014-05-12 11:06:54 +0100

    added bigdata java sand box

---
+++ bigdatastudies/src/main/java/com/cassio/studies/bigdata/App.java
@@ -0,0 +1,13 @@
+package com.cassio.studies.bigdata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
